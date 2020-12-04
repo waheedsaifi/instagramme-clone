9e73e524f7bad7867765096cbace9d1b0adca21f
waheedSaifi <waheed.saifi565@gmail.com>
2020-12-04 16:55:14 +0500

    my commit

---
+++ instagrame-clone/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Helo Waheed Ahmed Saifi!!!</h1>
+
+  {/* header */}
+
+  {/* post */}
+  {/* post */}
+  {/* post */}
+
+   </div>
+  );
+}
+
+export default App;
